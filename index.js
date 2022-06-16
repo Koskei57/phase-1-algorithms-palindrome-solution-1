650199834e305ce3a5647d64a7bce2b3d058fd90
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-16 22:27:01 +0300

    commit tree a53eb6676e6041194038071e1df0ea3bc5d04b30

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
+function reverseString(word) {
+    return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+    const reverseWord = reverseString(word);
+    return word === reverseWord;
+
 }
 
 /* 
   Add your pseudocode here
+reverse the input string
+if the reverse string is the same as the input
+return true
+else
+return false
+
 */
 
 /*
   Add written explanation of your solution here
+our code simply creates an array from the input string,reverses the keyed in array
+returns a true if the now reversed is the same as the input word in the array
+and returns false otherwise. 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("EXpecting: true");
+    console.log("=>", isPalindrome("a"));
+    console.log("");
 
-  console.log("");
+    console.log("EXpecting: true");
+    console.log("=>", isPalindrome("abba"));
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("EXpecting: false");
+    console.log("=>", isPalindrome("dab"));
+    console.log("");
+
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
+
+    console.log("");
+
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
